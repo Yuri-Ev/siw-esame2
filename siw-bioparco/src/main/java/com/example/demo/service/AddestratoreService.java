dc9d3759f7c013627928c1aabe2601f9f3bbbb22
Yuri <yur.evangelista@stud.uniroma3.it>
2022-07-06 22:58:18 +0200

    setupv2

---
+++ siw-bioparco/src/main/java/com/example/demo/service/AddestratoreService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.AddestratoreRepository;
+
+@Service
+public class AddestratoreService {
+
+	AddestratoreRepository addestratoreRepository;
+}
