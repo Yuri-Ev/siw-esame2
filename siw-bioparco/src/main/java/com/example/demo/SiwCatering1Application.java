96d66edd097d535d40a4d4672d349a684006d297
Yuri <yur.evangelista@stud.uniroma3.it>
2022-07-06 13:00:45 +0200

    setup

---
+++ siw-bioparco/src/main/java/com/example/demo/SiwCatering1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwCatering1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwCatering1Application.class, args);
+	}
+
+}
