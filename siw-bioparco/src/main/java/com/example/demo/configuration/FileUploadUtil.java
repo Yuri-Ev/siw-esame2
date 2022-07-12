ea3f5e1d0e6ce48c25cbe3d6ad19ae9fae0373d2
Yuri <yur.evangelista@stud.uniroma3.it>
2022-07-12 12:55:25 +0200

    updateFileUtil package

--- siw-bioparco/src/main/java/com/example/demo/configuration/FileUploadUtil.java
+++ siw-bioparco/src/main/java/com/example/demo/configuration/FileUploadUtil.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.configuration;
 
 import java.io.IOException;
 import java.io.InputStream;
--- siw-bioparco/src/main/java/com/example/demo/controller/AnimaleController.java
+++ siw-bioparco/src/main/java/com/example/demo/controller/AnimaleController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.demo.configuration.FileUploadUtil;
 import com.example.demo.model.Animale;
 import com.example.demo.service.AmbienteService;
 import com.example.demo.service.AnimaleService;
